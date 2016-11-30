9a16bd9d472b261bcfb85546c3ee89e5d90cee9c
hcarline <huntercarline@HUNTERCARLIC6AC>
2016-11-30 17:30:13 -0600

    rating is still reaming but also so is github

---
+++ CHY_Project/CHY_Project/Models/SongViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CHY_Project.Models;
+
+namespace CHY_Project.Models
+{
+    public class SongViewModel
+    {
+        public decimal AvgRating { get; set; }
+
+        public virtual List<Song> Songs { get; set; }
+        public virtual List<Rating> Ratings { get; set; }
+
+    }
+}
