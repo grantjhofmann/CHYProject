eb7c5812021bd86b37bee9ed8cff9bfe997f0704
grantjhofmann <ghofmann@utexas.edu>
2016-11-30 15:04:03 -0600

    begun work on checkout viewmodel

--- CHY_Project/CHY_Project/Models/CheckoutViewModel.cs
+++ CHY_Project/CHY_Project/Models/CheckoutViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace CHY_Project.Models
 {
     public class CheckoutViewModel
     {
+
+        public String CardNumber { get; set; }
+        public List<Product> Products { get; set; }
+
+        [Display (Name = "Is this a gift for someone? If so, check yes and give the recipient's email address below:")]
+        public Boolean Gift { get; set; }
+
+
+        [EmailAddress (ErrorMessage = "Enter a valid email address.")]
+        [Display (Name = "Recipient Email Address")]
+        public String RecipientEmail { get; set; }
     }
 }
