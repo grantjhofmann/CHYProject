076920c12836215d5572ed8af21198648839f847
grantjhofmann <ghofmann@utexas.edu>
2016-12-02 02:34:29 -0600

    my insides are melting

---
+++ CHY_Project/CHY_Project/Models/ReportViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CHY_Project.Models
+{
+    public class AlbumReportViewModel
+    {
+        public String AlbumName { get; set; }
+        public Decimal Revenue { get; set; }
+        public Int32 SalesCount { get; set; }
+    }
+
+    public class SongReportViewModel
+    {
+        public String SongName { get; set; }
+        public Decimal Revenue { get; set; }
+        public Int32 SalesCount { get; set; }
+    }
+
+    public class GenreReportViewModel
+    {
+
+    }
+}
